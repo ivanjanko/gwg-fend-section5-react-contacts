52907c650c63d3b5ef5c593e6e91fd1136e8e4c6
ivan Jankovic <jankoemail@gmail.com>
2018-10-04 15:42:27 -0400

    clicking on +person icon dosent routh

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 
 class CreateContact extends Component {
     render() {
         return (
-            <div>Create Contact!</div>
+            <div>
+                <Link className='close-create-contact' to='/'>Close</Link>
+            </div>
         )
     }
 }
